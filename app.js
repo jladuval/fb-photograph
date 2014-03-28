06380e94b027e0d0a7d99da0dad346afaae34ee4
jladuval <jladuval@gmail.com>
2014-03-29 09:06:08 +1300

    changed to take a stream instead of a photo path

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var FormData = require('form-data');
 
-exports.uploadPhoto = function(user_id, access_token, photo_path, message, cb){
+exports.uploadPhoto = function(user_id, access_token, photo_stream, message, cb){
 	var form = new FormData(); 
-	form.append('file', fs.createReadStream(photo_path));
+	form.append('file', photo_steam);
 	form.append('message', message);
 	form.append('access_token', access_token);
 	
